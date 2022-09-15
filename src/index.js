13b94664c5d5b63f58c39cfa3aaf3f636e5afe2c
elderflowerlime <antczak.julia96@gmail.com>
2022-09-15 21:05:25 +0200

    cosmetic changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import createRoot from 'react-dom';
+import './fonts/Audrey-Normal.otf';
 import App from './App';
+
 
 createRoot.render(<App />, document.getElementById('root'));
 
